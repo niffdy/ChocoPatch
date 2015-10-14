826d98d19371c872029095f6ff97d8dc4f82821a
Brad Sjue <bsjue@cerner.com>
2015-10-14 15:00:35 -0500

    Added download from URL
    Added the appmatch.csv download from a URL.

--- ChocoPatch.ps1
+++ ChocoPatch.ps1
@@ -1,9 +1,13 @@
 ﻿# to do 
 # test if chocolatey is installed, if not install it
-# pull csv from github url
+
+$url = "https://raw.githubusercontent.com/niffdy/ChocoPatch/master/appmatch.csv"
+$csvapplist = "$env:TEMP\appmatchtemp.csv"
+# $csvapplist
+(New-Object System.Net.WebClient).DownloadFile($url, $csvapplist)
 
 $installedapps = gp HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | ? {![string]::IsNullOrWhiteSpace($_.DisplayName) } | Select DisplayName, DisplayVersion, Publisher, InstallDate, HelpLink, UninstallString -Unique| sort DisplayName 
-$appmatch  =  ((get-content "S:\PowerShell\appmatch.csv") -replace ",","=") -join "`n" | convertfrom-stringdata
+$appmatch  =  ((get-content $csvapplist) -replace ",","=") -join "`n" | convertfrom-stringdata
 $cupblock="cup "
 
 foreach ($thisapp in $installedapps.DisplayName) {
@@ -12,8 +16,4 @@ foreach ($thisapp in $installedapps.DisplayName) {
  }
  $cupblock =  $cupblock + " -y"
  $cupblock
-# Invoke-Command -ScriptBlock $cupblock
 cmd /c $cupblock
-# $p = Start-Process choco -ArgumentList $cupblock -wait -NoNewWindow -WindowStyle Normal
-# $p.HasExited
-# $p.ExitCode
