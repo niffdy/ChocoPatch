f6bf7aac9b8080a2773c96af8b1d503dc1e857ba
Brad Sjue <bsjue@cerner.com>
2015-10-14 14:30:55 -0500

    Application Matching Files
    This maps locally installed applications to packages stored in
    https://chocolatey.org/packages

---
+++ ChocoPatch.ps1
@@ -0,0 +1,19 @@
+﻿# to do 
+# test if chocolatey is installed, if not install it
+# pull csv from github url
+
+$installedapps = gp HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | ? {![string]::IsNullOrWhiteSpace($_.DisplayName) } | Select DisplayName, DisplayVersion, Publisher, InstallDate, HelpLink, UninstallString -Unique| sort DisplayName 
+$appmatch  =  ((get-content "S:\PowerShell\appmatch.csv") -replace ",","=") -join "`n" | convertfrom-stringdata
+$cupblock="cup "
+
+foreach ($thisapp in $installedapps.DisplayName) {
+   If($appmatch.ContainsKey($thisapp) -eq $True)
+    {$cupblock=$cupblock+ ' ' + $appmatch.Get_Item($thisapp)}
+ }
+ $cupblock =  $cupblock + " -y"
+ $cupblock
+# Invoke-Command -ScriptBlock $cupblock
+cmd /c $cupblock
+# $p = Start-Process choco -ArgumentList $cupblock -wait -NoNewWindow -WindowStyle Normal
+# $p.HasExited
+# $p.ExitCode
